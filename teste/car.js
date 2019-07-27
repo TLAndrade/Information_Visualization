448d2b344dddda09db4b0c0db86f7b89570bbcd0
TLAndrade <42185482+TLAndrade@users.noreply.github.com>
2019-07-27 16:20:48 -0300

    simple page using CSS and SVG

---
+++ teste/car.js
@@ -0,0 +1,4 @@
+ const car= `[{"year":2000,"make":"honda","model":"Accord","price":2800},{"make":"Nissan","model":"F150","year":2009,"price":1950},{"year":1980,"make":"chevrolet","model":"HB20","price":15000}]`
+
+export let cars = JSON.parse(`[{"year":2000,"make":"honda","model":"Accord","price":2800},{"make":"Nissan","model":"F150","year":2009,"price":1950},{"year":1980,"make":"chevrolet","model":"HB20","price":15000}]`);
+
---
+++ teste/index.js
@@ -0,0 +1,5 @@
+import {cars} from './car.js';
+
+const m = 'Testing interation';
+
+document.getElementById('car-message').textContent = m;
